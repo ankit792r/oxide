0b613ec794893e7456dcd5aef4e2066d668b524b
Ankit Prajapati <ap8424856@gmail.com>
2025-11-09 23:35:48 +0530

    reveted nvidia conf

--- install/config/all.sh
+++ install/config/all.sh
@@ -5,5 +5,5 @@ source $OXIDE_INSTALL/config/theme.sh
 source $OXIDE_INSTALL/config/hardware/bluetooth.sh
 source $OXIDE_INSTALL/config/hardware/intel.sh
 source $OXIDE_INSTALL/config/hardware/network.sh
-# source $OXIDE_INSTALL/config/hardware/nvidia.sh
-source $OXIDE_INSTALL/config/hardware/usb.sh
+source $OXIDE_INSTALL/config/hardware/nvidia.sh
+# source $OXIDE_INSTALL/config/hardware/usb.sh
