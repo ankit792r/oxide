8fa0842a95a04e2e13077866a1c493ee132d8161
Ankit Prajapati <ap8424856@gmail.com>
2025-11-09 19:34:33 +0530

    updated helpers

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,2 +1,2 @@
-source $OXIDE_INSTALL/helpers/presentation.sh
 source $OXIDE_INSTALL/helpers/prepare.sh
+source $OXIDE_INSTALL/helpers/presentation.sh
--- install/helpers/prepare.sh
+++ install/helpers/prepare.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env bash
-
-clear_logo
 info "Preparing system..."
 gum spin --spinner dot --title "Updating system and installing essentials..." -- \
   sudo pacman -Syu --noconfirm && \
-  sudo pacman -S --noconfirm --needed base-devel git curl wget unzip
+  sudo pacman -S --noconfirm --needed base-devel git curl wget unzip gum
 
 success "System ready to install."
