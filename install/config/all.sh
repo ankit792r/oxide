d34a43556f9157ca31f6875b58a16cdba036be75
Ankit Prajapati <ap8424856@gmail.com>
2025-11-15 10:20:54 +0530

    commented hardware

--- install/config/all.sh
+++ install/config/all.sh
@@ -2,8 +2,8 @@ source $OXIDE_INSTALL/config/config.sh
 source $OXIDE_INSTALL/config/media.sh
 source $OXIDE_INSTALL/config/theme.sh
 
-source $OXIDE_INSTALL/config/hardware/bluetooth.sh
-source $OXIDE_INSTALL/config/hardware/intel.sh
-source $OXIDE_INSTALL/config/hardware/network.sh
+# source $OXIDE_INSTALL/config/hardware/bluetooth.sh
+# source $OXIDE_INSTALL/config/hardware/intel.sh
+# source $OXIDE_INSTALL/config/hardware/network.sh
 # source $OXIDE_INSTALL/config/hardware/nvidia.sh
 # source $OXIDE_INSTALL/config/hardware/usb.sh
